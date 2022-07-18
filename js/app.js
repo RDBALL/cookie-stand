07afc7c11bb9693d72762557158157d195fa20ea
Robert Ball <bobball89@gmail.com>
2022-07-18 15:09:03 -0700

    added dubai section

--- js/app.js
+++ js/app.js
@@ -92,17 +92,65 @@ let Tokyo = {
 Tokyo.render();
 console.log(Tokyo);
 
-// let dubai = {
-//   min: 11,
-//   max: 38,
-//   Avg: 3.7,
-// }
-// let paris = {
+// Dubai Section
+
+let Dubai = {
+  location: 'Dubai',
+  min: 11,
+  max: 38,
+  Avg: 3.7,
+  cookiesArr: [],
+  totalCookies: 0,
+  randomCustNumber: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  cookiesPerHour: function () {
+
+    for (let i = 0; i < hours.length; i++) {
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      this.cookiesArr.push(cookies);
+      this.totalCookies += cookies
+    }
+  },
+  render: function () {
+    this.cookiesPerHour();
+    let storeName = document.createElement('h3');
+    storeName.textContent = this.location; 
+    cookieStore.appendChild(storeName);
+    let ulElem = document.createElement('ul');
+    cookieStore.appendChild(ulElem);
+
+    for (let i = 0; i < this.cookiesArr.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = hours[i] + ': ' + this.cookiesArr[i] + ' cookies';
+      ulElem.appendChild(liElem);
+      console.log('total: ', this.totalCookies);
+    }
+    let totalElm = document.createElement('li');
+    totalElm.textContent = `total: ${this.totalCookies} cookies`;
+    ulElem.appendChild(totalElm);
+  }
+};
+
+Dubai.render();
+console.log(Dubai);
+
+
+
+
+
+
+
+
+
+// let Paris = {
+//  location: 'Paris',
 //   min: 20,
 //   max: 38,
 //   Avg: 2.3,
 // }
-// let lima = {
+// let Lima = {
+//   location: 'Paris',
 //   min: 2,
 //   max: 16,
 //   Avg: 4.6,
