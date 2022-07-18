956b485a4392baf1c279a9f7b10a798326464efc
Robert Ball <bobball89@gmail.com>
2022-07-18 15:14:45 -0700

    added lima section

--- js/app.js
+++ js/app.js
@@ -21,15 +21,15 @@ let Seattle = {
   cookiesPerHour: function () {
 
     for (let i = 0; i < hours.length; i++) {
-      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()));
       this.cookiesArr.push(cookies);
-      this.totalCookies += cookies
+      this.totalCookies += cookies;
     }
   },
   render: function () {
     this.cookiesPerHour();
     let storeName = document.createElement('h3');
-    storeName.textContent = this.location; 
+    storeName.textContent = this.location;
     cookieStore.appendChild(storeName);
     let ulElem = document.createElement('ul');
     cookieStore.appendChild(ulElem);
@@ -64,15 +64,15 @@ let Tokyo = {
   cookiesPerHour: function () {
 
     for (let i = 0; i < hours.length; i++) {
-      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()));
       this.cookiesArr.push(cookies);
-      this.totalCookies += cookies
+      this.totalCookies += cookies;
     }
   },
   render: function () {
     this.cookiesPerHour();
     let storeName = document.createElement('h3');
-    storeName.textContent = this.location; 
+    storeName.textContent = this.location;
     cookieStore.appendChild(storeName);
     let ulElem = document.createElement('ul');
     cookieStore.appendChild(ulElem);
@@ -107,15 +107,15 @@ let Dubai = {
   cookiesPerHour: function () {
 
     for (let i = 0; i < hours.length; i++) {
-      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()));
       this.cookiesArr.push(cookies);
-      this.totalCookies += cookies
+      this.totalCookies += cookies;
     }
   },
   render: function () {
     this.cookiesPerHour();
     let storeName = document.createElement('h3');
-    storeName.textContent = this.location; 
+    storeName.textContent = this.location;
     cookieStore.appendChild(storeName);
     let ulElem = document.createElement('ul');
     cookieStore.appendChild(ulElem);
@@ -150,15 +150,15 @@ let Paris = {
   cookiesPerHour: function () {
 
     for (let i = 0; i < hours.length; i++) {
-      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()));
       this.cookiesArr.push(cookies);
-      this.totalCookies += cookies
+      this.totalCookies += cookies;
     }
   },
   render: function () {
     this.cookiesPerHour();
     let storeName = document.createElement('h3');
-    storeName.textContent = this.location; 
+    storeName.textContent = this.location;
     cookieStore.appendChild(storeName);
     let ulElem = document.createElement('ul');
     cookieStore.appendChild(ulElem);
@@ -178,10 +178,45 @@ let Paris = {
 Paris.render();
 console.log(Paris);
 
+// Lima Section
 
-// let Lima = {
-//   location: 'Paris',
-//   min: 2,
-//   max: 16,
-//   Avg: 4.6,
-// }
+let Lima = {
+  location: 'Lima',
+  min: 2,
+  max: 16,
+  Avg: 4.6,
+  cookiesArr: [],
+  totalCookies: 0,
+  randomCustNumber: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  cookiesPerHour: function () {
+
+    for (let i = 0; i < hours.length; i++) {
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()));
+      this.cookiesArr.push(cookies);
+      this.totalCookies += cookies;
+    }
+  },
+  render: function () {
+    this.cookiesPerHour();
+    let storeName = document.createElement('h3');
+    storeName.textContent = this.location;
+    cookieStore.appendChild(storeName);
+    let ulElem = document.createElement('ul');
+    cookieStore.appendChild(ulElem);
+
+    for (let i = 0; i < this.cookiesArr.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = hours[i] + ': ' + this.cookiesArr[i] + ' cookies';
+      ulElem.appendChild(liElem);
+      console.log('total: ', this.totalCookies);
+    }
+    let totalElm = document.createElement('li');
+    totalElm.textContent = `total: ${this.totalCookies} cookies`;
+    ulElem.appendChild(totalElm);
+  }
+};
+
+Lima.render();
+console.log(Lima);
