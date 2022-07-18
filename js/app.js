3509b26a3f6aa2237c9bf7107b57cb790e72ebff
Robert Ball <bobball89@gmail.com>
2022-07-18 15:04:11 -0700

    added tokyo section

--- js/app.js
+++ js/app.js
@@ -6,10 +6,7 @@ let cookieStore = document.getElementById('Seattle-total');
 
 console.log(cookieStore);
 
-// // returns random number betwwen or equal to minCust / maxCust
-// function randCustomers(min, max){
-//   return Math.floor(Math.random() * (max - min +1) + min);
-// }
+// Seattle Section
 
 let Seattle = {
   location: 'Seattle',
@@ -52,32 +49,61 @@ let Seattle = {
 Seattle.render();
 console.log(Seattle);
 
+// Tokyo Section
 
-// let tokyo = {
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookieSale: 1.2,
-// }
+let Tokyo = {
+  location: 'Tokyo',
+  min: 3,
+  max: 24,
+  Avg: 1.2,
+  cookiesArr: [],
+  totalCookies: 0,
+  randomCustNumber: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  cookiesPerHour: function () {
+
+    for (let i = 0; i < hours.length; i++) {
+      let cookies = (Math.ceil(this.Avg * this.randomCustNumber()))
+      this.cookiesArr.push(cookies);
+      this.totalCookies += cookies
+    }
+  },
+  render: function () {
+    this.cookiesPerHour();
+    let storeName = document.createElement('h3');
+    storeName.textContent = this.location; 
+    cookieStore.appendChild(storeName);
+    let ulElem = document.createElement('ul');
+    cookieStore.appendChild(ulElem);
+
+    for (let i = 0; i < this.cookiesArr.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = hours[i] + ': ' + this.cookiesArr[i] + ' cookies';
+      ulElem.appendChild(liElem);
+      console.log('total: ', this.totalCookies);
+    }
+    let totalElm = document.createElement('li');
+    totalElm.textContent = `total: ${this.totalCookies} cookies`;
+    ulElem.appendChild(totalElm);
+  }
+};
+
+Tokyo.render();
+console.log(Tokyo);
+
 // let dubai = {
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookieSale: 3.7,
+//   min: 11,
+//   max: 38,
+//   Avg: 3.7,
 // }
 // let paris = {
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCookieSale: 2.3,
+//   min: 20,
+//   max: 38,
+//   Avg: 2.3,
 // }
 // let lima = {
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCookieSale: 4.6,
-// }
-
-// Class demo for lab
-// let hours = ['6am', '7am', '8am'....'7pm']
-//let seattle {
-//   minCust: 23,
-//   maxCust: 65,
-//   avgCookie: 6.3,
+//   min: 2,
+//   max: 16,
+//   Avg: 4.6,
 // }
