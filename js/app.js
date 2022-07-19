e155a7657758b14bd1abefcf28b85cf25e32a5bb
Robert Ball <bobball89@gmail.com>
2022-07-19 11:01:21 -0700

    created prototypes, added table section to sales html

--- js/app.js
+++ js/app.js
@@ -20,9 +20,54 @@ function StoreLocation(location, min, max, avg) {
   this.totalCookies = 0;
 }
 
+// Creating Prototypes
 
+StoreLocation.prototype.randomCustNumber = function() {
+  return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+};
 
+StoreLocation.prototype.cookiesPerHour = function(){
+  for (let i = 0; i < hours.length; i ++) {
+    let cookies = (Math.ceil(this.avg * this.randomCustNumber()));
+    console.log('cookies', cookies);
+    this.cookiesArr.push(cookies);
+    this.totalCookies += cookies;
+  }
+};
 
+StoreLocation.prototype.render = function() {
+  this.cookiesPerHour();
+  let row1 = document.createElement('tr');
+  locationTable.appendChild(row1);
+
+  let tdElem = document.createElement('td');
+  tdElem.textContent = this.location;
+  row1.appendChild (tdElem);
+
+  for (let i =0; i < this.cookiesArr.length; i++) {
+    let td = document.createElement('td');
+    td.textContent = this.cookiesArr[i];
+    row1.appendChild(td);
+    console.log('total: ', this.totalCookies);
+  };
+  let totalElm = document.createElement ('thead');
+  locationTable.appendChild(tableHead);
+
+  let row = document.createElement('tr');
+  tableHead.appendChild(row);
+
+  let tdElem = document.createElement('td');
+  row.appendChild(tdElem);
+
+  for (let i = 0; i < hours.length; i++){
+    let td = document.createElement('td');
+    td.textContent = hours[i];
+    row.appendChild(td);
+  }
+  let total = document.createElement('td');
+  total.textContent = 'Total';
+  row.appendChild(total);
+}
 
 
 
